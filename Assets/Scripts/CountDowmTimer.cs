9bd06122401de5c7d4ef06c567524ad5c9fc8d72
ChavezPaulina <77360224+ChavezPaulina@users.noreply.github.com>
2021-03-13 23:24:51 -0600

    Pins&needles

---
+++ Assets/Scripts/CountDowmTimer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CountDowmTimer : MonoBehaviour
+{
+    public string levelToLoad;
+    private float timer = 10f;
+    private Text timerSeconds;
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f2");
+        if (timer <= 0)
+        {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/PinSpeed.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinSpeed : MonoBehaviour
+{
+    public float minSpeed = 100f;
+    public float maxSpeed = 800f;
+    public Rigidbody2D rb;
+    float speed = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = Random.Range(minSpeed, maxSpeed);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 forward = new Vector2(transform.right.x, transform.right.y);
+        rb.MovePosition(rb.position + forward * Time.deltaTime * speed);
+    }
+
+    public void AdjustSpeed (float newSpeed)
+    {
+        speed = newSpeed;
+    }
+}
